3799364261bf2efa7c77375be41df2492ad8b3a4
almeidaadev <unkn0wn01127800@gmail.com>
2024-06-08 23:14:42 -0300

    up

---
+++ src/script/index.js
@@ -0,0 +1,27 @@
+//carousel
+
+const imgs = document.querySelectorAll(".imgs img");
+
+// O indece 0 [ e a seta pra esquerda]
+const arrowsCarousel = document.querySelectorAll(`.arrows-carousel i`);
+
+const pictures = [
+  `./assets/imgsCarousel/Rectangle 18.png`,
+  `./assets/imgsCarousel/Group 44.png`,
+  `./assets/imgsCarousel/unsplash_519_90ncAds.png`,
+];
+
+arrowsCarousel[0].addEventListener("click", () => {
+  for (let i = 0; i < pictures.length; i++) {
+    imgs[i].src = pictures[i];
+  }
+
+  pictures.unshift(pictures.pop());
+});
+
+arrowsCarousel[1].addEventListener("click", () => {
+  for (let i = 0; i < pictures.length; i++) {
+    imgs[i].src = pictures[i];
+  }
+  pictures.push(pictures.shift());
+});
