3391d174abe70e696307a3c426b8327035763276
almeidaadev <unkn0wn01127800@gmail.com>
2024-06-14 13:53:17 -0300

    addNewfunctionalities

--- dist/index.ce517d76.js
+++ dist/index.ce517d76.js
@@ -1,11 +1,11 @@
 //carousel
 const imgs = document.querySelectorAll(".imgs img");
 // O indece 0 [ e a seta pra esquerda]
-const arrowsCarousel = document.querySelectorAll(`.arrows-carousel i`);
+const arrowsCarousel = document.querySelectorAll(`.arrows-carousel img`);
 const pictures = [
-    "./assets/imgsCarousel/Rectangle 18.png",
-    "./assets/imgsCarousel/Group 44.png",
-    "./assets/imgsCarousel/unsplash_519_90ncAds.png"
+    "/Rectangle%2018.a6d2bbec.png",
+    "/Group%2044.23c3d097.png",
+    "/unsplash_519_90ncAds.39b6dc5d.png"
 ];
 arrowsCarousel[0].addEventListener("click", ()=>{
     for(let i = 0; i < pictures.length; i++)imgs[i].src = pictures[i];
@@ -15,5 +15,8 @@ arrowsCarousel[1].addEventListener("click", ()=>{
     for(let i = 0; i < pictures.length; i++)imgs[i].src = pictures[i];
     pictures.push(pictures.shift());
 });
+imgs[0].src = pictures[0];
+imgs[1].src = pictures[1];
+imgs[2].src = pictures[2];
 
 //# sourceMappingURL=index.ce517d76.js.map
--- src/script/index.js
+++ src/script/index.js
@@ -3,12 +3,12 @@
 const imgs = document.querySelectorAll(".imgs img");
 
 // O indece 0 [ e a seta pra esquerda]
-const arrowsCarousel = document.querySelectorAll(`.arrows-carousel i`);
+const arrowsCarousel = document.querySelectorAll(`.arrows-carousel img`);
 
 const pictures = [
-  "./assets/imgsCarousel/Rectangle 18.png",
-  "./assets/imgsCarousel/Group 44.png",
-  "./assets/imgsCarousel/unsplash_519_90ncAds.png",
+  "/Rectangle%2018.a6d2bbec.png",
+  "/Group%2044.23c3d097.png",
+  "/unsplash_519_90ncAds.39b6dc5d.png",
 ];
 
 arrowsCarousel[0].addEventListener("click", () => {
@@ -25,3 +25,7 @@ arrowsCarousel[1].addEventListener("click", () => {
   }
   pictures.push(pictures.shift());
 });
+
+imgs[0].src = pictures[0];
+imgs[1].src = pictures[1];
+imgs[2].src = pictures[2];
